75c8e3c92c8bb79c73862f773bca0ef80a58bc09
Halozraa <revvv03@gmail.com>
2024-12-20 20:21:34 +0700

    Add hash with PEPPER

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express"
 import bodyParser from "body-parser"
 import pool from "./db.js"
+import bcrypt from "bcrypt"
 
 const app = express()
+const PEPPER = "iloveu3000years"
 
 app.set("views engine", "ejs")
 app.use(express.static("public"))
@@ -27,12 +29,27 @@ app.get("/regis", (req, res)=>{
 app.post("/regis", async (req, res)=>{
     try {
         const {email,password} = req.body
-        console.log(email,password)
-        await pool.query("INSERT INTO users(email,password) VALUES($1,$2)",[email,password])
+
+        // Check email from database
+        const selectEmail = await pool.query("SELECT * FROM users WHERE email=$1",[email])
+        if (selectEmail.rows.length > 0){
+            console.log("Email used")
+            return res.status(400).send("email sudah digunakan")
+        }
+
+        // hash pass input user
+        const passPepper = password+ PEPPER
+        const saltRound = 10 ;
+        const hashPass = await bcrypt.hash(passPepper,saltRound)
+
+        // post email and pass to database
+        await pool.query("INSERT INTO users(email,password) VALUES($1,$2)",[email,hashPass])
         console.log("Berhasil regis")
         res.redirect("/")
+
     } catch (error) {
-        console.log(error)
+        console.error("Error saat regis", error.message)
+        res.status(500).json("Terjadi kesalahan pada server")
     }
 
 
@@ -41,20 +58,31 @@ app.post("/regis", async (req, res)=>{
 app.post("/login", async (req, res)=>{
     try {
         const {email,password} = req.body
-        console.log(email,password)
+
+        // check email dari database
         const data = await pool.query("SELECT * FROM users WHERE email=$1",[email])
-        if (data.rows.length===0){
+        if (data.rows.length === 0){
             return res.status(404).json("email tidak ditemukan")
         }
+        // mengambil pass dari database
         const dataSu = data.rows[0]
         const pass = dataSu.password
-        if(pass=== password){
+
+        // hash password input
+        const passWithPepper = password + PEPPER
+
+        //bandikan password input dengan di database
+        const isMatch = await bcrypt.compare(passWithPepper,pass)
+
+        // check validation
+        if(isMatch){
             res.status(200).json("Berhasil login")
         }else{
-            res.send("password wrong")
+            res.status(401).send("password salah")
         }
     } catch (error) {
-        console.log(error)
+        console.error("error saat login",error.message)
+        res.status(500).json("Ada kesalahan di server")
     }
 
 
